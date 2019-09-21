b0890b00b16a9d3e1818a4ecd08e6c7e6625eea8
Joanna Zhou <joanna.zyz@outlook.com>
2019-09-21 11:20:23 -0400

    Finished the histogram equalization

--- Image-transformation-billboard-hacking/templates/dlt_homography.py
+++ Image-transformation-billboard-hacking/templates/dlt_homography.py
@@ -21,22 +21,22 @@ def dlt_homography(I1pts, I2pts):
     """
     #--- FILL ME IN ---
 
-    # initialize the 8*9 matrix stacked by 4 2*9 matrixes
+    # Initialize the 8*9 matrix stacked by 4 2*9 matrixes
     A = []
 
     for i in range(0, len(I1pts[0])):
         # c[u v 1]^T = H[x y 1]^T where c is a non-zero constant
-        # in other words, coordinates of one point in I1pts -> (x, y), I2pts -> (u, v)
+        # In other words, coordinates of one point in I1pts -> (x, y), I2pts -> (u, v)
         x, y = I1pts[0][i], I1pts[1][i]
         u, v = I2pts[0][i], I2pts[1][i]
         A.append([-x, -y, -1, 0, 0, 0, u*x, u*y, u])
         A.append([0, 0, 0, -x, -y, -1, v*x, v*y, v])
 
     # H is obtained by Ah = 0, solved by SVD
-    # the solution is V's last column (the column corresponding to sigma=0)
+    # The solution is V's last column (the column corresponding to sigma=0)
     U, S, V = np.linalg.svd(np.array(A))
 
-    # normalize the matrix so that it is homogenous (i.e. the bottom-right is 1)
+    # Normalize the matrix so that it is homogenous (i.e. the bottom-right is 1)
     H = V[-1,:].reshape(3, 3)/ V[-1,-1]
 
     #------------------
--- Image-transformation-billboard-hacking/templates/histogram_eq.py
+++ Image-transformation-billboard-hacking/templates/histogram_eq.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def histogram_eq(I):
     """
@@ -21,6 +23,24 @@ def histogram_eq(I):
     # Verify I is grayscale.
     if I.dtype != np.uint8:
         raise ValueError('Incorrect image format!')
+
+    # Flatten the 2D matrix to a 1D array
+    I_flat = I.flatten()
+
+    # Initiate and update the histogram of "counts of pixels at 256 intensity levels"
+    histogram = np.zeros(256)
+    for pixel in I_flat:
+        histogram[pixel] += 1
+
+    # Compute the cumulative distribution function at each intensity level, stored in a list
+    cdf = [histogram[0]]
+    for bar in histogram:
+        cdf.append(cdf[-1] + bar)
+    cdf = np.array(cdf)*255/len(I_flat)
+
+    # Transform the new pixel intensities to J which has the same shape of I
+    J_flat = cdf[I_flat]
+    J = np.reshape(J_flat, I.shape).astype('uint8')
 
     #------------------
 
