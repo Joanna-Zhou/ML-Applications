718dc963d9e97d0af2a7a454cc1c8dc6c73dd84f
Joanna Zhou <joanna.zyz@outlook.com>
2019-10-20 09:29:18 -0400

    Finished the basic stereo matching algorithm

--- stereo-matching/templates/stereo_disparity_fast.py
+++ stereo-matching/templates/stereo_disparity_fast.py
@@ -46,57 +46,54 @@ def stereo_disparity_fast(Il, Ir, bbox, maxd):
 
     #--- FILL ME IN ---
 
-    def add_padding(I, padding):
-        """Helper function, copy the border pixels to the image paddings"""
-        # print(I.shape)
-        I = np.hstack((I[:, :padding], I)) # the left columns
-        I = np.hstack((I, I[:, -padding:])) # the right columns
-        I = np.vstack((I[:padding, :], I)) # the top rows
-        I = np.vstack((I, I[-padding:, :])) # the bottom rows
-        # print(I.shape)
-        return I
-
-
-    # Deifne parameters
-    kernel_size = 10
-    half_kernel = int(kernel_size/2)
-
-    # Initialize Id
-    Id = np.zeros(Il.shape)
-    Il, Ir = add_padding(Il, half_kernel), add_padding(Ir, half_kernel)
-
-    # Preprocess the images by adding paddings
-    # Recall: x axis >>, y axis vv, I[y, x]
-    print("\n\n\n############################################################\n")
-    print("Bbox:", bbox)
-    x_r1, x_r2 = half_kernel, Ir.shape[-1]+half_kernel
-    x_l1, x_l2 = min(bbox[0, 0], bbox[0, 1])+half_kernel,  max(bbox[0, 0], bbox[0, 1])+1+half_kernel
-    y_1, y_2 = min(bbox[1, 0], bbox[1, 1])+half_kernel,  max(bbox[1, 0], bbox[1, 1]+1)+1+half_kernel
-    Il, Ir = add_padding(Il, half_kernel), add_padding(Ir, half_kernel)
-
-    # Loop through the images and fill in Id
-    for row in range(y_1, y_2):
-        # Get all the windows from the right image along this row, stored in window_r_list as np.arrays
-        window_r_list = [] # will be 1 * width of r
-        for col_r in range(x_r1, x_r2):
-            window_r = Ir[row-half_kernel:row+half_kernel+1, col_r-half_kernel:col_r+half_kernel+1]
-            window_r_list.append(window_r)
-
-        print("Size of window_r_list:", len(window_r_list))
-        # Loop through all the windows from the left image and store their correspondending x_r
-        for col_l in range(x_l1, x_l2):
-            SAD_list = [] # will be 1 * maxd
-            window_l = Il[row-half_kernel:row+half_kernel+1, col_l-half_kernel:col_l+half_kernel+1]
-            # Get this window's corresponding x_r thru comparing with the right windows
-            # starting from x_l and ends at maxd pixels to the right of it
-            print(len(window_r_list[col_l-x_r1-maxd : col_l-x_r1]))
-            for window_r in window_r_list[col_l-x_r1-maxd : col_l-x_r1]:
-                SAD_list.append(np.mean(np.absolute(window_l-window_r)))
-            print("Size of SAD_list is {} at column {}".format(col_l, len(SAD_list)))
-            # Compute disparsity = x_l - x_r
-            x_r = np.argmin(np.array(SAD_list)) + col_l - maxd
-            disparsity = col_l - x_r
-            Id[row-half_kernel][col_l-half_kernel] = disparsity
+    # def add_padding(I, padding):
+    #     """Helper function, copy the border pixels to the image paddings"""
+    #     # print(I.shape)
+    #     I = np.hstack((I[:, :padding], I)) # the left columns
+    #     I = np.hstack((I, I[:, -padding:])) # the right columns
+    #     I = np.vstack((I[:padding, :], I)) # the top rows
+    #     I = np.vstack((I, I[-padding:, :])) # the bottom rows
+    #     # print(I.shape)
+    #     return I
+    #
+    #
+    # # Deifne parameters
+    # kernel_size = 13
+    # half_kernel = int(kernel_size/2)
+    # maxd -= 1
+    #
+    # # Initialize Id
+    # Id = np.zeros(Il.shape)
+    # Il, Ir = add_padding(Il, half_kernel), add_padding(Ir, half_kernel)
+    #
+    # # Preprocess the images by adding paddings
+    # # Recall: x axis >>, y axis vv, I[y, x]
+    # x_r1, x_r2 = half_kernel, Ir.shape[-1]+half_kernel
+    # x_l1, x_l2 = min(bbox[0, 0], bbox[0, 1])+half_kernel,  max(bbox[0, 0], bbox[0, 1])+1+half_kernel
+    # y_1, y_2 = min(bbox[1, 0], bbox[1, 1])+half_kernel,  max(bbox[1, 0], bbox[1, 1]+1)+1+half_kernel
+    # Il, Ir = add_padding(Il, half_kernel), add_padding(Ir, half_kernel)
+    #
+    # # Loop through the images and fill in Id
+    # for row in range(y_1, y_2):
+    #     # Get all the windows from the right image along this row, stored in window_r_list as np.arrays
+    #     window_r_list = [] # will be 1 * width of r
+    #     for col_r in range(x_r1, x_r2):
+    #         window_r = Ir[row-half_kernel:row+half_kernel+1, col_r-half_kernel:col_r+half_kernel+1]
+    #         window_r_list.append(window_r)
+    #
+    #     # Loop through all the windows from the left image and store their correspondending x_r
+    #     for col_l in range(x_l1, x_l2):
+    #         SAD_list = [] # will be 1 * maxd
+    #         window_l = Il[row-half_kernel:row+half_kernel+1, col_l-half_kernel:col_l+half_kernel+1]
+    #         # Get this window's corresponding x_r thru comparing with the right windows
+    #         # starting from x_l and ends at maxd pixels to the right of it
+    #         for window_r in window_r_list[col_l-x_r1-maxd : col_l-x_r1+1]:
+    #             SAD_list.append(np.mean(np.absolute(window_l-window_r)))
+    #
+    #         # Compute disparsity = x_l - x_r
+    #         x_r = np.argmin(np.array(SAD_list)) + col_l - maxd
+    #         disparsity = col_l - x_r
+    #         Id[row-half_kernel][col_l-half_kernel] = disparsity
 
     #------------------
 
