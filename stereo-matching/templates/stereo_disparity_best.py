35c9b151dacebd675fbe06cda357d5f9f0ef7fa8
Joanna Zhou <joanna.zyz@outlook.com>
2019-10-21 23:23:01 -0400

    Finished the LBP algorithmm needs further tuning

--- stereo-matching/templates/stereo_disparity_best.py
+++ stereo-matching/templates/stereo_disparity_best.py
@@ -46,171 +46,236 @@ def stereo_disparity_best(Il, Ir, bbox, maxd):
     #  - Don't optimize for runtime (too much), optimize for clarity.
 
     #--- FILL ME IN ---
+    # http://courses.csail.mit.edu/18.337/2010/projects/reports/Kaneva_final_report.pdf
 
+
+    # Parameters that require tuning ###########################################################################
     # Parameters
-    Lambda=maxd
-    nb_iterations=120
+    maxiter = 100 # max number of belief propagation iterations
+    tau = 15. # for the data cost
+    lamb = maxd # the impact of smoothness in the cost function
+    K = 2. # only when using the truncated linear model fpr smoothness
+    sigma = 0.1 # gaussian pamareter for the input images before stereo matching
 
-    def compute_data_cost(I1, I2, maxd, Tau):
-        """data_cost: a 3D array of sixe height x width x num_disp_value;
-        data_cost(y,x,l) is the cost of assigning the label l to pixel (y,x).
-        The cost is min(1/3 ||I1[y,x]-I2[y,x-l]||_1, Tau)."""
-        h,w,_ = I1.shape
-        dataCost=np.zeros((h,w,maxd))
+    # Preprocessing
+    print("Processing LBP with {} iterations, lamb={}, and tau={}:".format(maxiter, lamb, tau))
+    m, n = Il.shape # m=height(#rows) and n=width(#columns)
+    Il, Ir = gaussian_filter(Il, sigma), gaussian_filter(Ir, sigma) # smoothen the img
 
-        for lp in range(maxd):
-            # print(I1 - np.roll(I2, lp, axis=1))
-            # print(I1.shape, np.roll(I2, lp, axis=1).shape)
-            dataCost[:, :, lp] = np.minimum(1./3*np.linalg.norm(I1 - np.roll(I2, lp, axis=1), axis=2, ord=1), Tau*np.ones((h, w)))
 
-        return dataCost
+    # Helper functions used in the Loopy Belief Propagation #####################################################
+    def pass_messages(messages, direction="from"):
+        """
+        Helper function, takes in a bundle of 4 messages and pass to/from the up/down/left/right neighbours
 
-    def compute_energy(dataCost,disparity,Lambda):
-        """dataCost: a 3D array of sixe height x width x maxd;
-        dataCost(y,x,l) is the cost of assigning the label l to pixel (y,x).
-        disparity: array of size height x width containing disparity of each pixel.
-        (an integer between 0 and maxd-1)
-        Lambda: a scalar value.
-        Return total energy, a scalar value"""
-        h,w,maxd = dataCost.shape
+        Parameters:
+        -----------
+        messages    - 4 x (m x n x maxd) or 4 x (m x n), a list of the messages of thr 4 neighbours, to be updated
+                      note that an intensity map which doesn't have the "maxd" dimension can also be processed with it
+        direction   - "from" or "to", specifying if the rolling is in forward (to) or backward (from) direction
+                      in most of the cases such as belief and message propagation, we only need "from"
 
-        hh, ww = np.meshgrid(range(h), range(w), indexing='ij')
-        dplp = dataCost[hh, ww, disparity]
+        Returns:
+        --------
+        messages    - Same as input dimension, the "rolled" messages
+        """
+        if direction == "from":
+            msg_down, msg_up,  msg_right, msg_left = messages
+        else:
+            msg_up, msg_down, msg_left, msg_right = messages
+        msg_up = np.roll(msg_up, 1, axis=0)
+        msg_down = np.roll(msg_down, -1, axis=0)
+        msg_left = np.roll(msg_left, 1, axis=1)
+        msg_right = np.roll(msg_right, -1, axis=1)
 
-        # Unitary cost of assigning this disparity to each pixel
-        energy = np.sum(dplp)
+        return msg_up, msg_down, msg_left, msg_right
 
-        # Compute interaction cost of each neighbors
-        interactionCostU = Lambda*(disparity - np.roll(disparity, 1, axis=0) != 0)
-        interactionCostL = Lambda*(disparity - np.roll(disparity, 1, axis=1) != 0)
-        interactionCostD = Lambda*(disparity - np.roll(disparity, -1, axis=0) != 0)
-        interactionCostR = Lambda*(disparity - np.roll(disparity, -1, axis=1) != 0)
+    def data_cost_table(Il, Ir, maxd=maxd, tau=tau):
+        """
+        Helper function, computes the truncated absolute intensity difference
+        as the cost of assigning disparity d to pixel p
+        It is used as a D_cost lookup table for a given disparity mapping
 
-        # Ignoring edge costs
-        interactionCostU[0, :] = 0
-        interactionCostL[:, 0] = 0
-        interactionCostD[-1, :] = 0
-        interactionCostR[:, -1] = 0
+        Parameters:
+        -----------
+        Il, Ir  - m x n x 1 (broadcasted)
+        maxd    - maximum disparity value; disparities d's are taken in range(0, maxd)
 
-        # Adding interaction cost of each neighbors
-        energy += np.sum(interactionCostU)
-        energy += np.sum(interactionCostL)
-        energy += np.sum(interactionCostD)
-        energy += np.sum(interactionCostR)
+        Returns:
+        --------
+        D_costs - m x n x maxd, the data cost d_cost for each disparity d, on each pixel p
+                  where d_cost = min(|Il(y,x)-Ir[y,x-d]|, tau)
+        """
+        D_costs = np.zeros((m, n, maxd))
+
+        for d in range(maxd):
+            # Along x direction, shift each pixel by d to get Ir[y,x-d]
+            # Note that np.roll is used to prevent values beyound x's range
+            # Alternatively, we can do a subtraction and then turn values <0 or >n-1 into 0 and n-1
+            Ir_shifted = np.roll(Ir, d, axis=1)
+            D_costs[:, :, d] = np.minimum(np.abs(Il - Ir_shifted), tau*np.ones((m, n)))
+        return D_costs
+
+    def smoothness_cost(Id1, Id2, neighbour, model="Potts", lamb=lamb):
+        """
+        Helper function, computes the smoothness cost given two neighbours
+
+        Parameters:
+        -----------
+        Id1     - m x n, disparity mapping 1
+        Id2     - m x n, disparity mapping 2
+        neighbour   - string, specifying which neightbour it is
+        model   - string, either "Potts" or "Linear"
+        lamb    - scaler, smoothness parameter,the bigger, the smoother
+
+        Returns:
+        --------
+        S_cost  - scaler, sum of all S(p1-p2) = {p1=p2: 0, otherwise: 1}
+        """
+        # Get smoothness cost of the neighbour in each direction by the Potts or truncated linear model
+        if model == "Potts":
+            S_cost = (Id1 - Id2)!=0
+        else:
+            S_cost = np.minimum(np.abs(Id1 - Id2), K*np.ones((m, n)))
+
+        # The extra edge "rolled" by the shift is turned back to its original values
+        if neighbour == 'up':
+            S_cost[0, :] = 0
+        elif neighbour == 'down':
+            S_cost[-1, :] = 0
+        elif neighbour == 'left':
+            S_cost[:, 0] = 0
+        else:
+            S_cost[:, -1] = 0
+
+        return S_cost * float(lamb)
+
+    def smoothness_cost_total(Id, model="Potts", lamb=lamb):
+        """
+        Helper function, computes the total smoothness cost (i.e. left/right/up/down) at given Id map
+        Note that there are many different models, and here the Potts model us used to reduce computation
+
+        Parameters:
+        -----------
+        Id      - m x n, disparity mapping
+        model   - string, either "Potts" or "Linear"
+        lamb    - scaler, smoothness parameter,the bigger, the smoother
+
+        Returns:
+        --------
+        S_cost  - scaler, sum of all S(p1-p2) = {p1=p2: 0, otherwise: 1}
+        """
+        # Disparity map of the current Id shifted by 1 in each direction
+        Id_up, Id_down, Id_left, Id_right = pass_messages([Id, Id, Id, Id], direction='to')
+
+        S_cost_up = smoothness_cost(Id, Id_up, 'up', model, lamb)
+        S_cost_down = smoothness_cost(Id, Id_down, 'down', model, lamb)
+        S_cost_left = smoothness_cost(Id, Id_left, 'left', model, lamb)
+        S_cost_right = smoothness_cost(Id, Id_right, 'right', model, lamb)
+
+        # Sum up the total cost across the image for each direction
+        S_cost = (np.sum(S_cost_up)+
+                  np.sum(S_cost_down)+
+                  np.sum(S_cost_left)+
+                  np.sum(S_cost_right))
+
+        return S_cost
+
+    def total_energy(Id, D_costs, lamb=lamb):
+        """
+        Helper function, returns the total energy at the current disparity mapping
+        Note that this is the objective function to be minimized w.r.t. disparity (Id)!
+
+        Parameters:
+        -----------
+        Id, D_costs, and lamb are the same inputs as the above functions
+
+        Returns:
+        --------
+        energe  - scaler, sum of data cost and smoothness cost computed by the above helper functions
+        """
+        # Finds the individual data costs given the current disparity mapping, and add them together
+        data_cost = 0
+        for i in range(m):
+            for j in range(n):
+                data_cost += D_costs[i, j, Id[i, j]]
+
+        energy = data_cost + smoothness_cost_total(Id, model="Potts")
 
         return energy
 
-    def update_msg(msgUPrev,msgDPrev,msgLPrev,msgRPrev,dataCost,Lambda):
-        """Update message maps.
-        dataCost: 3D array, depth=label number.
-        msgUPrev,msgDPrev,msgLPrev,msgRPrev: 3D arrays (same dims) of old messages.
-        Lambda: scalar value
-        Return msgU,msgD,msgL,msgR: updated messages"""
-        msgU=np.zeros(dataCost.shape)
-        msgD=np.zeros(dataCost.shape)
-        msgL=np.zeros(dataCost.shape)
-        msgR=np.zeros(dataCost.shape)
+    def propagate(messages, D_costs, lamb=lamb, model="Potts"):
+        """
+        Helper function, computes the total smoothness cost (i.e. left/right/up/down) at given Id map
+        Note that there are many different models, and here the Potts model us used to reduce computation
 
-        h,w,maxd = dataCost.shape
+        Parameters:
+        -----------
+        messages - 4 x (m x n x maxd), a list of the messages of thr 4 neighbours, to be updated
+        D_costs and lamb are the same as in all the other functions
 
-        msg_incoming_from_U = np.roll(msgDPrev, 1, axis=0)
-        msg_incoming_from_L = np.roll(msgRPrev, 1, axis=1)
-        msg_incoming_from_D = np.roll(msgUPrev, -1, axis=0)
-        msg_incoming_from_R = np.roll(msgLPrev, -1, axis=1)
+        Returns:
+        --------
+        messages - The updated list of messages
+        """
 
-        npqU = dataCost + msg_incoming_from_L + msg_incoming_from_D + msg_incoming_from_R
-        npqL = dataCost + msg_incoming_from_U + msg_incoming_from_D + msg_incoming_from_R
-        npqD = dataCost + msg_incoming_from_L + msg_incoming_from_U + msg_incoming_from_R
-        npqR = dataCost + msg_incoming_from_L + msg_incoming_from_D + msg_incoming_from_U
+        # Messages pasaed from it's neighbours previously
+        msg_up_prev, msg_down_prev, msg_left_prev, msg_right_prev = pass_messages(messages)
 
-        spqU = np.amin(npqU, axis=2)
-        spqL = np.amin(npqL, axis=2)
-        spqD = np.amin(npqD, axis=2)
-        spqR = np.amin(npqR, axis=2)
+        # The data cost and previous messages, using the min-sum model
+        energy_up = D_costs + msg_left_prev + msg_down_prev + msg_right_prev
+        energy_left = D_costs + msg_up_prev + msg_down_prev + msg_right_prev
+        energy_down = D_costs + msg_left_prev + msg_up_prev + msg_right_prev
+        energy_right = D_costs + msg_left_prev + msg_down_prev + msg_up_prev
+        min_energy_up = np.amin(energy_up, axis=2)
+        min_energy_left = np.amin(energy_left, axis=2)
+        min_energy_down = np.amin(energy_down, axis=2)
+        min_energy_right = np.amin(energy_right, axis=2)
 
-        for lp in range(maxd):
-            msgU[:, :, lp] = np.minimum(npqU[:, :, lp], Lambda + spqU)
-            msgL[:, :, lp] = np.minimum(npqL[:, :, lp], Lambda + spqL)
-            msgD[:, :, lp] = np.minimum(npqD[:, :, lp], Lambda + spqD)
-            msgR[:, :, lp] = np.minimum(npqR[:, :, lp], Lambda + spqR)
+        # Initialize and update the new messages: new msg at this neighbour with Id1 is given by
+        #   min{D(Id2) + S(Id1, Id2) + sum(previous msgs except this one itself)}
+        msg_up,msg_down,msg_left,msg_right= np.zeros((m, n, maxd)), np.zeros((m, n, maxd)), \
+                                            np.zeros((m, n, maxd)), np.zeros((m, n, maxd))
+        for d in range(maxd):
+            msg_up[:, :, d] = np.minimum(energy_up[:, :, d], min_energy_up + \
+                              smoothness_cost(min_energy_up,msg_up_prev[:, :, d], 'up', model, lamb))
+            msg_left[:, :, d] = np.minimum(energy_left[:, :, d], min_energy_left + \
+                                smoothness_cost(min_energy_left,msg_left_prev[:, :, d], 'down', model, lamb))
+            msg_down[:, :, d] = np.minimum(energy_down[:, :, d], min_energy_down + \
+                                smoothness_cost(min_energy_down,msg_down_prev[:, :, d], 'left', model, lamb))
+            msg_right[:, :, d] = np.minimum(energy_right[:, :, d], min_energy_right + \
+                                 smoothness_cost(min_energy_right,msg_right_prev[:, :, d], 'right', model, lamb))
 
-        return msgU,msgD,msgL,msgR
+        # Normalize each message pack, so that they have 0 as mean, otherwise they'd all grow huge
+        messages = [msg_up,msg_down,msg_left,msg_right]
+        for idx, msg in enumerate(messages):
+            mean = np.mean(msg, axis=2)
+            messages[idx] -= mean[:, :, np.newaxis]
+        return messages
 
-    def normalize_msg(msgU,msgD,msgL,msgR):
-        """Subtract mean along depth dimension from each message"""
+    # The main body of Loopy Belief Propagation #################################################################
+    # Precompute the D_cost table
+    D_costs = data_cost_table(Il, Ir, maxd)
 
-        avg=np.mean(msgU,axis=2)
-        msgU -= avg[:,:,np.newaxis]
-        avg=np.mean(msgD,axis=2)
-        msgD -= avg[:,:,np.newaxis]
-        avg=np.mean(msgL,axis=2)
-        msgL -= avg[:,:,np.newaxis]
-        avg=np.mean(msgR,axis=2)
-        msgR -= avg[:,:,np.newaxis]
+    # Initiate messages from up, down, left, and right
+    # Use np.ones if sum-product is used insteaf of min-sum
+    messages = [np.zeros((m, n, maxd))] * 4
+    energy = []
 
-        return msgU,msgD,msgL,msgR
+    # Perform the LBP to minimize the energy
+    for i in range(maxiter):
+        print('Iteration {}'.format(i))
+        messages = propagate(messages, D_costs, model="Linear")
+        # energy.append(total_energy(Id, D_costs)) # used to debug and observe, commented out for saving time
 
-    def compute_belief(dataCost,msgU,msgD,msgL,msgR):
-        """Compute beliefs, sum of data cost and messages from all neighbors"""
-        beliefs=dataCost.copy()
+    # Once the max number of iteration is reached, we get the belief from the updated messages
+    msg_up, msg_down, msg_left, msg_right = pass_messages(messages)
+    Beliefs = D_costs + msg_up + msg_down + msg_left + msg_right
 
-        msg_incoming_from_U = np.roll(msgD, 1, axis=0)
-        msg_incoming_from_L = np.roll(msgR, 1, axis=1)
-        msg_incoming_from_D = np.roll(msgU, -1, axis=0)
-        msg_incoming_from_R = np.roll(msgL, -1, axis=1)
+    # The final guess of Id is the disparsity mapping which yields the least cost (i.e. Beliefs)
+    Id = np.argmin(Beliefs, axis=2)
 
-        beliefs += msg_incoming_from_D + msg_incoming_from_L + msg_incoming_from_R + msg_incoming_from_U
-
-        return beliefs
-
-    def MAP_labeling(beliefs):
-        """Return a 2D array assigning to each pixel its best label from beliefs
-        computed so far"""
-        return np.argmin(beliefs, axis=2)
-
-    def stereo_bp(I1,I2,maxd,Lambda,Tau=15,num_iterations=60):
-        """The main function"""
-        dataCost = compute_data_cost(I1, I2, maxd, Tau)
-        energy = np.zeros((num_iterations)) # storing energy at each iteration
-        # The messages sent to neighbors in each direction (up,down,left,right)
-        h,w,_ = I1.shape
-        msgU=np.zeros((h, w, maxd))
-        msgD=np.zeros((h, w, maxd))
-        msgL=np.zeros((h, w, maxd))
-        msgR=np.zeros((h, w, maxd))
-
-        print('Iteration (out of {}) :'.format(num_iterations))
-        for iter in range(num_iterations):
-            print('\t'+str(iter))
-            msgU,msgD,msgL,msgR = update_msg(msgU,msgD,msgL,msgR,dataCost,Lambda)
-            msgU,msgD,msgL,msgR = normalize_msg(msgU,msgD,msgL,msgR)
-            # Next lines unused for next iteration, could be done only at the end
-            beliefs = compute_belief(dataCost,msgU,msgD,msgL,msgR)
-            disparity = MAP_labeling(beliefs)
-            energy[iter] = compute_energy(dataCost,disparity,Lambda)
-
-        return disparity,energy
-
-    def to_rgb(im):
-        # as 1, but we use broadcasting in one line
-        w, h = im.shape
-        ret = np.empty((w, h, 3), dtype=np.uint8)
-        ret[:, :, :] = im[:, :, np.newaxis]
-        return ret
-
-
-    # Convert as float gray images
-    Il = to_rgb(Il)
-    Ir = to_rgb(Ir)
-    Il=Il.astype(float)
-    Ir=Ir.astype(float)
-
-    # Gaussian filtering
-    I1=gaussian_filter(Il, 0.6)
-    I2=gaussian_filter(Ir, 0.6)
-
-    Id,energy = stereo_bp(I1,I2,maxd,Lambda, num_iterations=nb_iterations)
-    imwrite('disparity_{:g}.png'.format(Lambda),Id)
+    imwrite('disparity_{:g}.png'.format(lamb),Id)
 
     #------------------
 
@@ -229,10 +294,10 @@ if __name__ == '__main__':
 
     # Il = imread("../images/teddy_image_02.png", as_gray = True)
     # Ir = imread("../images/teddy_image_06.png", as_gray = True)
-    bbox = np.array(bboxes["teddy_02"]["bbox"])
+    # bbox = np.array(bboxes["teddy_02"]["bbox"])
 
     Id = stereo_disparity_best(Il, Ir, bbox, 52)
     print("Score:", stereo_disparity_score(It, Id, bbox))
-    plt.imshow(Id, cmap = "gray")
-    plt.show()
+    # plt.imshow(Id, cmap = "gray")
+    # plt.show()
 ###################################################################
